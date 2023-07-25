c3cf3b68851a9dfc43d6ad71b8671b251a0e5063
A. Shustovsky <a.shustovsky@gmail.com>
2023-07-25 13:57:52 +0400

    fix: add react-dom

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
-import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { StrictMode } from 'react';
 import { App } from './App.tsx';
 import './index.scss';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </StrictMode>
 );
